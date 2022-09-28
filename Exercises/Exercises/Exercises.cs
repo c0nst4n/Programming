87be16df8d3eb6eb2df7b4c92bd41ec7fb200711
c0nst4n <costacostero@gmail.com>
2022-09-28 16:45:57 +0200

    1era cosa

---
+++ Exercises/Exercises/Exercises.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercises
+{
+    internal class Exercises
+    {
+        public static bool IsEven(int a)
+        {
+            return a % 2 == 0;
+        }
+
+        public static bool IsCousin(int a)
+        {
+
+            for (int i = 2; i < a; i++)
+            {
+                if ((a % i) == 0)
+                    return false;
+
+            }
+
+            return true;
+        }
+
+        public static double GetAreaCir(double rad)
+        {
+
+            return Math.PI * (rad * rad);
+        }
+
+        public static double GetAreaRec(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double GetDistance(double ax, double bx, double ay, double by)
+        {
+           
+        }
+    }
+
+}
