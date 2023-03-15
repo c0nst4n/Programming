2b488ad5b86a24b8dba8987a400e9c3079060a87
c0nst4n <costacostero@gmail.com>
2023-03-15 12:14:21 +0100

    15-03-2023

---
+++ EV3/MontyHall/MontyHall/Game.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontyHall
+{
+   public enum GameResult
+    {
+        WIN,
+        LOSE
+    }
+    enum Door
+    {
+        CAR,
+        GOAT
+    }
+    public class Game
+    {
+        private List<Door> _doorList = new List<Door>();
+        private Random _rand= new Random();
+        public void Init()
+        {
+           _doorList.Add(Door.CAR);
+           _doorList.Add(Door.GOAT);
+           _doorList.Add(Door.GOAT);
+        }
+
+
+        public GameResult Execute(bool wantsChange)
+        {
+            Init();
+            int select1 = _rand.Next(0, _doorList.Count - 1);
+            
+
+            if (_doorList[select1] == Door.GOAT && wantsChange)
+            {
+                _doorList.RemoveAt(select1);
+              int select2 = _rand.Next(0, _doorList.Count - 1);
+
+                if (_doorList[select2] == Door.GOAT)
+                    return GameResult.LOSE;
+                if (_doorList[select2] == Door.CAR)
+                    return GameResult.WIN;
+            }
+            int personalChoice = _rand.Next(0, _doorList.Count - 1);
+            if (_doorList[personalChoice] == Door.CAR)
+                return GameResult.WIN;
+
+            return GameResult.LOSE;
+        }
+    }
+}
