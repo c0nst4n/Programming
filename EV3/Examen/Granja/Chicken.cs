8a8556684d95c14cfd08e9c933f199d6ff7d8aa9
Javier Marín <56446732+jmarinprofe@users.noreply.github.com>
2023-06-06 06:26:18 +0200

    Update Chicken.cs

--- EV3/Examen/Granja/Chicken.cs
+++ EV3/Examen/Granja/Chicken.cs
@@ -9,7 +9,7 @@ namespace Granja
 {
     public class Chicken :Flying, ITerrestial 
     {
-        
+        // Javi: Faltan cosas
         public Chicken(bool IsVaccined, int id, Zone zone, double weight, Date eggDate) : base(IsVaccined, id, zone, weight, eggDate)
         { 
         }
